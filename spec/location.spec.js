c99ed0115e884f5a7d63134f619031289f722f18
joe <joseph.beuckman@usbank.com>
2016-10-03 14:25:26 -0500

    testing send geoheader to server for nearby locations

--- spec/location.spec.js
+++ spec/location.spec.js
@@ -25,24 +25,40 @@ describe('locations', function () {
         request(server)
             .post('/location')
             .send(loc)
-            .expect(201, done);
+            .expect(201)
+            .end(function(err, resp){
+                expect(err).toBeNull();
+            
+                location_id = resp._id;
+            });
     });
     
     
     it('can get nearby locations', function(done) {
         
-        var loc = {
-            latitude: 44.9,
-            longitude: -93.3
+        var geo_header = {
+            "Geo-Position": "44.9,-93.3 epu=50 hdn=45 spd=15"
         };
         
         request(server)
             .get('/location')
+            .set(geo_header)
             .end(function(err, resp){
                 expect(err).toBeNull();
             
                 expect(resp.content).toBeArray();
             });
+        
+    });
+    
+    it('can delete a location', function(done){
+        
+        request(server)
+            .delete('/location/'+location_id)
+            .end(function(err, resp){
+                expect(err).toBeNull();
+            });
+        
         
     });
         
