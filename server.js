21163a744ffbca4f152c32a6519702228b6f4122
joe <joseph.beuckman@usbank.com>
2016-10-03 14:08:53 -0500

    failing test for get geonear locations

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express  = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-mongoose.connect(process.env.MONGODB_URI, function (error) {
+var db_url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+
+mongoose.connect(db_url, function (error) {
     if (error) console.error(error);
     else console.log('mongo connected');
 });
--- spec/location.spec.js
+++ spec/location.spec.js
@@ -1,3 +1,4 @@
+require('jasmine-expect');
 var request = require('supertest');
 
 describe('locations', function () {
@@ -12,14 +13,37 @@ describe('locations', function () {
         server.close();
     });
 
-    it('post a location', function(done) {
+    it('can post a location', function(done) {
         
-        var loc = {};
+        var loc = {
+            description: 'test',
+            longitude: 1,
+            latitude: 1,
+            status: 'test'
+        };
         
         request(server)
             .post('/location')
             .send(loc)
             .expect(201, done);
+    });
+    
+    
+    it('can get nearby locations', function(done) {
+        
+        var loc = {
+            latitude: 44.9,
+            longitude: -93.3
+        };
+        
+        request(server)
+            .get('/location')
+            .end(function(err, resp){
+                expect(err).toBeNull();
+            
+                expect(resp.content).toBeArray();
+            });
+        
     });
         
 });
