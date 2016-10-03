2ca8dcd8f190e8f9334bd9b4055d07701fc30505
Joe Beuckman <joebeuckman@gmail.com>
2016-10-02 22:06:01 -0500

    trying a db doc

--- resources/location.js
+++ resources/location.js
@@ -4,6 +4,11 @@ module.exports.post = function (req, res) {
     var loc = new Location(req.body);
 
     loc.save(function(err, resp) {
+        
+        if (err) {
+            return res.status(500).json(err);
+        }
+        
         res.status(201).json(resp);
     });
 };
