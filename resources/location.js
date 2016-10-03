0ad7375893146212ccea3eb5a4882517c4eba29a
Joe Beuckman <joebeuckman@gmail.com>
2016-10-02 22:03:59 -0500

    trying a db doc

--- resources/location.js
+++ resources/location.js
@@ -1,6 +1,9 @@
 var Location = require('../models/location');
 
-module.exports.post = function(req, res) {
-    console.log(req.body);
-    res.status(201).json({});
+module.exports.post = function (req, res) {
+    var loc = new Location(req.body);
+
+    loc.save(function(err, resp) {
+        res.status(201).json(resp);
+    });
 };
