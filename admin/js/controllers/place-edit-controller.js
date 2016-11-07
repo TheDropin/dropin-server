ede9d6a9eb6ac5a3d22e37d547b72e22b301953d
joe beuckman <joebeuckman@gmail.com>
2016-11-06 19:06:32 -0600

    live updating place icon on edit page

--- admin/js/controllers/place-edit-controller.js
+++ admin/js/controllers/place-edit-controller.js
@@ -1,44 +1,65 @@
 angular.module('dropinAdmin')
-.controller('PlaceEditController', function($scope, DropinService, $stateParams){
-    console.log('PDC: '+JSON.stringify($stateParams));
-    
-    $scope.place = $stateParams.place;
+    .controller('PlaceEditController', function ($scope, DropinService, $stateParams) {
+        console.log('PDC: ' + JSON.stringify($stateParams));
 
-    var placeholder;
-    
-    function addPlaceholderMarker(place) {
+        $scope.place = $stateParams.place;
 
-        var myLatLng = {
-            lat: place.geometry.coordinates[0],
-            lng: place.geometry.coordinates[1]
-        };
+        var placeholder;
 
-        var placeholder = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            draggable: true,
-            //            icon: PaletteService.getMapPinForStop(stop),
-            title: 'Hello World!'
+        function addPlaceholderMarker(place) {
+            console.log(place);
+            var myLatLng = {
+                lat: place.geometry.coordinates[0],
+                lng: place.geometry.coordinates[1]
+            };
+
+            var placeholder = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                draggable: true,
+                //            icon: PaletteService.getMapPinForStop(stop),
+                title: 'Hello World!'
+            });
+
+            $scope.$watch('place.type', function (value) {
+                console.log(value);
+
+                var iconUrl = DropinService.placeIcon(place.type);
+            if (iconUrl) {                
+                var image = {
+                    url: iconUrl,
+                    size: new google.maps.Size(32,32),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(13, 13),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+                placeholder.setIcon(iconUrl);
+            }
+            
+            });
+
+            return placeholder;
+        }
+
+        $scope.$on('MAP_LOADED', function (e, _map) {
+            map = _map;
+            $scope.map = map;
+
+            placeholder = addPlaceholderMarker($scope.place);
+            map.setCenter(placeholder.getPosition());
+
+            placeholder.addListener('dragend', function () {
+                map.panTo(placeholder.getPosition());
+            });
         });
 
-        return placeholder;
-    }
-    
-    $scope.$on('MAP_LOADED', function (e, _map) {
-        map = _map;
-        $scope.map = map;
-        
-        placeholder = addPlaceholderMarker($scope.place);
-        map.setCenter(placeholder.getPosition());
-        
-        placeholder.addListener('dragend', function(){
-            map.panTo(placeholder.getPosition());
-        });
+
+        $scope.save = function () {
+            console.log($scope.place);
+
+            var pos = placeholder.getPosition().toJSON();
+
+            $scope.place.geometry.coordinates = [pos.lat, pos.lng];
+        }
+
     });
-    
-    
-    $scope.savePlace = function() {
-        
-    }
-    
-});
