6d3fb2542b0bf02a4869ea880176f3df6385c72c
Joe Beuckman <joebeuckman@gmail.com>
2017-04-02 14:10:34 -0500

    initial commit

---
+++ models/Location.js
@@ -0,0 +1,32 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Location Model
+ * ==========
+ */
+
+var Location = new keystone.List('Location', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true },
+});
+
+Location.add({
+    nid: { type: Types.Number },
+	title: { type: String, required: true },
+    wkt: { type: String },
+    address: { type: String },
+    telephone: { type: String },
+	location: { type: Types.Location, defaults: { city: 'Minneapolis' } },
+	author: { type: Types.Relationship, ref: 'User', index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	image: { type: Types.CloudinaryImage },
+	content: { type: Types.Html, wysiwyg: true, height: 400 }
+});
+
+Location.schema.virtual('content.full').get(function () {
+	return this.content.extended || this.content.brief;
+});
+
+Location.defaultColumns = 'title, location|20%, author|20%, publishedDate|20%';
+Location.register();
---
+++ models/ServiceType.js
@@ -0,0 +1,27 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * ServiceType Model
+ * ==========
+ */
+
+var ServiceType = new keystone.List('ServiceType', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true },
+});
+
+ServiceType.add({
+    nid: { type: Types.Number },
+	title: { type: String, required: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	image: { type: Types.CloudinaryImage },
+	content: { type: Types.Html, wysiwyg: true, height: 400 }
+});
+
+ServiceType.schema.virtual('content.full').get(function () {
+	return this.content;
+});
+
+ServiceType.defaultColumns = 'title, location|20%, type|20%, publishedDate|20%';
+ServiceType.register();
